57ee44fa1a5923c42aa8cc8e8d78bbe63443e901
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-07-03 20:07:58 +0300

    fix jumpable interface

--- src/interfaces/IJumpable.ts
+++ src/interfaces/IJumpable.ts
@@ -1,8 +1,10 @@
 export interface IJumpable {
     getDuration(): number
+    getSpeed(): number
     getTime(): number
 
     setDuration(d: number): void
+    setSpeed(s: number): void
 
     jump(): void 
     is_jump(): number
