b30f6f84eb1c5fad9e4998baf19f6897b5767d8c
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-07-03 20:07:04 +0300

    add empty sprite

--- src/core/Objects/NoSprite.ts
+++ src/core/Objects/NoSprite.ts
@@ -1,8 +1,8 @@
-import Actions from "../../types/Actions";
+import ActionName from "../../types/ActionNames";
 import { Sprite } from "./Sprite";
 
 export class NoSprite extends Sprite {
     constructor(){
-        super('',new Map<Actions, [number, number]>([[Actions._0_Idle, [0, 0]]]),0)
+        super('',new Map<ActionName, [number, number]>([[ActionName._0_Idle, [0, 0]]]),0)
     }
 }
